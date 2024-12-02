55701bc7bd52df5ee96bedd4f1eaa5ec605e8824
jjalseu <cjfwns966@gmail.com>
2024-12-02 20:06:37 +0900

    - 퀴즈 등록,수정,상세,삭제 API

--- src/service/quiz/types.ts
+++ src/service/quiz/types.ts
@@ -37,3 +37,25 @@ export interface QuizItem {
     updatedAt: string;
     detailUrl:string // 상세 URL
 }
+
+
+// 퀴즈 요청 타입 & 퀴즈 폼 타입
+export interface QuizForm{
+    title:string, // 퀴즈 제목
+    content:string, // 퀴즈 내용
+    subjectiveAnswer:string, // 주관식 답안
+    multipleChoiceAnswer:number[], // 객관식 답안
+    hint:string,// 힌트
+    explanation:string,// 해설
+    type:QuizType,// 퀴즈 종류
+    field:string,// 분야
+    lang:string, // 언어
+    level:number, // 난이도
+    isMultiple:boolean,// 객관식일 경우 중복 선택 여부
+    time:number, // 시간
+    multipleChoiceContents:string[] //객관식 컨텐츠
+    metaTitle:string, // 제목
+    metaDescription:string, // 설명
+    metaImageUrl:string // 이미지 URL
+    detailUrl:string // 상세 URL
+}
