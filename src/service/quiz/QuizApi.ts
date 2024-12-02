c4c4d9cebb3cb7ef758b3fbe064fe72ee7c4cc99
jjalseu <cjfwns966@gmail.com>
2024-12-02 21:10:32 +0900

    -  퀴즈목록 endpoint 변경

--- src/service/quiz/QuizApi.ts
+++ src/service/quiz/QuizApi.ts
@@ -24,11 +24,11 @@ export interface AbstractQuizApi{
 class QuizApi extends BaseApi implements AbstractQuizApi{
 
     constructor() {
-        super(process.env.REACT_APP_API_ENDPOINT!); // BaseApi에 API 엔드포인트 전달
+        super(import.meta.env.VITE_API_ENDPOINT!); // BaseApi에 API 엔드포인트 전달
     }
     // 목록
     async getQuizList(request?:QuizListRequest): Promise<IResponse<QuizItem[]>>{
-        return this.request<QuizItem[]>("quiz", {
+        return this.request<QuizItem[]>("quiz/list", {
             method: "GET",
             queryString: request
         });
