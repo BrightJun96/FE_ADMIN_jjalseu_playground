1c26711103dc53fd133b340cb741726543dc2bb0
jjalseu <cjfwns966@gmail.com>
2024-12-02 16:03:11 +0900

    - API class 셋팅

--- src/service/network.types.ts
+++ src/service/network.types.ts
@@ -1,14 +1,14 @@
-// fetch option 타입 확장
+
+export type QueryString = string | URLSearchParams | Record<string, string> | string[][];
+
 export interface CustomRequestInit extends RequestInit {
-    queryString?:(string[][] | Record<string, string> | string | URLSearchParams)
-    // 인증이 필요한지에 대한 파라미터
-    auth?: boolean;
+    queryString?: QueryString;
 }
+
 
 // 응답 타입
-export interface IResponse<T>{
-    data:T;
+export interface IResponse<T> {
+    data: T;
+    message: string|null;
+    isSuccess: boolean;
 }
-
-
-
